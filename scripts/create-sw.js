f03bb003280886b47ccff4a999efb6e84b72e211
Luis <reallyluis@gmail.com>
2021-01-30 12:56:31 -0800

    #4 add generate service worker by workbox-build

---
+++ scripts/create-sw.js
@@ -0,0 +1,36 @@
+const workboxBuild = require('workbox-build');
+const args = process.argv.slice(2); 
+const dir = args[0] || 'build';
+
+// NOTE: This should be run *AFTER* all your assets are built
+const buildSW = () => {
+  // This will return a Promise
+  return workboxBuild.generateSW({
+    globDirectory: dir,
+    globPatterns: [
+      '**/*.{html,json,js,css}',
+    ],
+    swDest: `${dir}/sw.js`,
+
+    // Define runtime caching rules.
+    runtimeCaching: [{
+      // Match any request that ends with .png, .jpg, .jpeg or .svg.
+      urlPattern: /\.(?:png|jpg|jpeg|svg|mp4)$/,
+
+      // Apply a cache-first strategy.
+      handler: 'CacheFirst',
+
+      options: {
+        // Use a custom cache name.
+        cacheName: 'images',
+
+        // Only cache 10 images.
+        expiration: {
+          maxEntries: 10,
+        },
+      },
+    }],
+  });
+};
+
+buildSW();
